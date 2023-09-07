4750e87f0baaa6105c067b575a20c93a2299e0df
Maruf <aamaruf2000@gmail.com>
2023-09-07 08:36:59 +0600

    Added solution of problem4

--- Week- 2/Module- 6.5 (Practice Day 02)/problem4.py
+++ Week- 2/Module- 6.5 (Practice Day 02)/problem4.py
@@ -1,6 +1,7 @@
 
 """ 
-4. Create a chat bot that will understand if you are bored, then it will call the Bored API and suggest some activities to you in reply. It should also have the ability to tell the current time.
+4. Create a chat bot that will understand if you are bored, then it will call the Bored API and suggest some activities to you in reply.
+ It should also have the ability to tell the current time.
 
 Example:
 >> Hey robot, this house is boring. Any idea what I can do?
@@ -8,4 +9,29 @@ Bot:  You can learn Javascript.
 >> thank you. What’s the time?
 Bot: it’s 9:30 pm sir. 
 """
+
+import datetime
+import requests
+import time
+
+bored_msg = ["bore", "boring", "bored"]
+time_msg = ["time", "clock"]
+
+def get_activity():
+    response = requests.get("http://www.boredapi.com/api/activity/")
+    data = response.json()
+    return data["activity"]
+while True:
+    user_msg = input(">> ")
+    for word in bored_msg:
+        if word in user_msg.lower():
+            activity = get_activity()
+            print("Bot :" + activity)
+    for word in time_msg:
+            if word in user_msg.lower():
+                now = datetime.datetime.now().strftime('%I:%M %p')
+                print("Bot : " + now)
+    time.sleep(1)
+
+
 
